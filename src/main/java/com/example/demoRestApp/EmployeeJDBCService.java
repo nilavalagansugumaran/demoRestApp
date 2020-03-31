08f5b8b2ab89a4ded89802c7a2143150d48fa6f2
Nilavalagan Sugumaran <>
2020-03-31 13:35:37 +0100

    Initial JDBC coomit

---
+++ src/main/java/com/example/demoRestApp/EmployeeJDBCService.java
@@ -0,0 +1,62 @@
+package com.example.demoRestApp;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Data
+@Service
+@Slf4j
+public class EmployeeJDBCService {
+
+   @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+//    public Employee getEmployee(long id) {
+//
+//
+//    }
+//
+//    public Employee addEmployee(Employee employee) {
+//
+//    }
+//
+//    public void deleteEmployee(long id) {
+//
+//    }
+//
+//    public void updateEmployee(long id, Employee employee) {
+//
+//
+//    }
+
+    @PostConstruct
+    public void setupDB() {
+
+        jdbcTemplate.execute("create table EMPLOYEES(" +
+                "employeeid int auto_increment, " +
+                "name varchar(50), " +
+                "salary double, " +
+                "primary key (employeeid) )");
+
+        jdbcTemplate.update("insert into EMPLOYEES (name, salary) " +
+                        "values (?, ?)",
+                new Object[]{"James-1", 21000, });
+
+        jdbcTemplate.update("insert into EMPLOYEES (name, salary) " +
+                        "values (?, ?)",
+                new Object[]{"James-2", 22000, });
+
+        jdbcTemplate.update("insert into EMPLOYEES (name, salary) " +
+                        "values (?, ?)",
+                new Object[]{"James-3", 23000, });
+
+    }
+}
